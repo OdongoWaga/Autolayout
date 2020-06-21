5bebf20ec878ed0dc1e46a189597732371f0e577
Waga <wagaodongo@gmail.com>
2020-06-21 18:45:52 +0300

    Initial Commit

---
+++ hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by JFJ on 21/06/2020.
+//  Copyright © 2020 JFJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
